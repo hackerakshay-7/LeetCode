682f46b00053f334c971f800e9cb2e9d9626a0cf
hackerakshay-7 <akz.ggtv@gmail.com>
2025-08-31 23:49:30 +0530

    Time: 363 ms (5.08%) | Memory: 64.2 MB (11.83%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPrimes(int n) {
+        // seive of eratosthenes by adarsh gupta
+        int[]arr = new int[n+1]; 
+        int count =0;
+        Arrays.fill(arr,1);
+        for(int i =2;i<=n/i;i++){
+            for(int j = i*i;j<n;j+=i){
+                if(j%i==0) arr[j]=0;
+            }
+        }
+        for(int i =2;i<n;i++){
+            if(arr[i]==1) count++;
+        }
+   return count; }
+}
