0087dca18b2592f55dd0adaf54e5ef4fe0ebcb74
Akshat <akz.ggtv@gmail.com>
2026-02-23 17:11:10 +0530

    Time: 1 ms (28.61%) | Memory: 47.1 MB (92.12%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,14 +1,16 @@
 class Solution {
+    void swap(int i,int j,int[]arr){
+       int temp =arr[i];
+       arr[i]=arr[j];
+       arr[j]=temp;
+    }
     public int missingNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int max = Integer.MIN_VALUE;
-        for(int a : nums){
-            set.add(a);
-            max = Math.max(max,a);
-        }
-        for(int i = max;i>-1;i--){
-            if(!set.contains(i)) return i;
-        }
-        
-   return max+1; }
+       int i=0;
+       while(i<nums.length){
+        if(nums[i]==i || nums[i]==nums.length){i++;}
+        else{swap(i,nums[i],nums);}}   
+   for(int j=0;j<nums.length;j++){
+    if(nums[j]!=j) return j;
+   }
+   return nums.length;}
 }
