44ba9958398ab3896ca5e33ef9aaffe97106d993
Akshat <akz.ggtv@gmail.com>
2026-03-01 11:39:21 +0530

    Time: 12 ms (6.81%) | Memory: 42.7 MB (10.13%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.java
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.java
@@ -1,6 +1,23 @@
 class Solution {
     public int minCost(int n) {
-        // minimum cost hoga agar 1 + (n-1) meh batoge;
-        return n*(n-1)/2;
-    }
+        int count=1,ans=0;
+        if(n==1) return 0;
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+            if(n%2==0){
+                pq.add(n/2);
+                pq.add(n/2);
+                ans+=((n/2)*(n/2));
+            }
+            else{pq.add((n/2)+1);pq.add(n/2);ans+=((n/2)*((n/2)+1));}
+            count++;
+            while(count!=n){
+                int p = pq.poll();
+                if(p==1) continue;
+                if(p%2==0){ pq.add(p/2);pq.add(p/2); 
+                 ans+=((p/2)*(p/2));}
+                else{pq.add((p/2)+1);pq.add(p/2);
+                ans+=((p/2)*((p/2)+1));}
+                count++;
+            }
+   return ans; }
 }
