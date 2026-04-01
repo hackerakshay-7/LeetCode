9d58e8b38c9508a080567d483cd66c06332c20f4
Akshat <akz.ggtv@gmail.com>
2026-04-02 00:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (44.36%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,12 +1,7 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int count = 0;
         // every trailing zero is made up of 5 X 2 
         // multiple of 2 occurs more frequently than multiple of 5
         // count total num. of multiple of 5 to get num of trailing zeros
-        while(n>0){
-            count+=n/5;
-            n/=5;
-        }
-   return count; }
+      return (n/15625 + n/3125 + n/625 + n/125 + n/25 + n/5); }
 }
