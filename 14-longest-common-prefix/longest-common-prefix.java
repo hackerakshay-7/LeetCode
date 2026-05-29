0fe891a79192539f5139b09c565e72322bea7a2a
Akshat <akz.ggtv@gmail.com>
2026-05-29 14:05:43 +0530

    Time: 4 ms (13.43%) | Memory: 43.8 MB (5.64%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,13 +1,49 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j - 1].charAt(i) != strs[j].charAt(i))
-                    return sb.toString();
-            }
-            sb.append(strs[0].charAt(i));
+    class Node {
+        Node[] children;
+        boolean isend;
+        int childcount;
+
+        Node() {
+            children = new Node[26];
+            isend = false;
         }
-        return sb.toString();
     }
+
+    Node root = new Node();
+
+    void insert(String word) {
+        Node temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (temp.children[idx] == null) {
+                temp.children[idx] = new Node();
+                temp.childcount++;
+            }
+            if (i == word.length() - 1)
+                temp.children[idx].isend = true; // last word inserted
+            temp = temp.children[idx];
+
+        }
+
+    }
+    String sol(String strs){
+        String ans ="";
+        Node t = root;
+        for(int i =0;i<strs.length();i++){
+            if(t.childcount==1) {ans+=strs.charAt(i);
+            int idx = strs.charAt(i)-'a';
+            t=t.children[idx];}
+            else break;
+            if(t.isend==true) break;
+        }
+        return ans;
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        for(int i =0;i<strs.length;i++){
+            if(strs[i].length()==0) return "";
+            insert(strs[i]);
+        }
+    return sol(strs[0]);}
 }
