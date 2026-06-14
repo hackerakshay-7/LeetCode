1ffbe2c971f08afa68c558d0ba8e7d130613e843
Akshat <akz.ggtv@gmail.com>
2026-06-14 11:59:49 +0530

    Time: 4 ms (98.30%) | Memory: 100.9 MB (90.30%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,17 +10,30 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        int max=Integer.MIN_VALUE;
-       Stack<Integer> st = new Stack<>();
-       ListNode temp=head;
-       while(temp!=null){
-        st.push(temp.val);
-        temp=temp.next;
-       }
-       int n = st.size();
-       for(int i=0;i<n/2;i++){
-        max=Math.max(max,(st.pop()+head.val));
-        head=head.next;
-       }
-    return max;}
+        ListNode slow = head;
+        ListNode fast = head;
+        // 1. finding middle element
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow = slow.next;
+        }
+        ListNode curr =slow;
+        ListNode prev = null;
+        //2. reverese the half of it
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        // prev is the head of the reversed half linked list (the list is individual)
+        // now add
+        int max =0;
+        while(prev!=null){
+            if((prev.val+head.val)>max) max = prev.val+head.val;
+            prev=prev.next;
+           head= head.next;
+        } 
+
+     return max;  }
 }
