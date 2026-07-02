6bd1939e7fabc39fdd0c7571dd87d716728a7dde
Akshat <akz.ggtv@gmail.com>
2026-07-02 11:02:07 +0530

    Time: 18 ms (57.57%) | Memory: 78.3 MB (34.95%) - LeetSync

---
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int matrixSum(int[][] nums) {
+        int ans =0;
+        for(int a[]: nums){
+            Arrays.sort(a);
+        }
+      for(int i =0;i<nums[0].length;i++){
+        int max =0;
+        for(int j =0;j<nums.length;j++){
+            max = Math.max(max,nums[j][i]);
+        }
+        ans+=max;
+      }
+   return ans; }
+}
