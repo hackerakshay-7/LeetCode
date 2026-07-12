cdad56885c55ee732e7a5f2f20e2fd7a706f8874
Akshat <akz.ggtv@gmail.com>
2026-07-12 19:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,27 +1,28 @@
 class Solution {
-    private int house(int idx , int [] arr,Integer [] dp){
-        if(idx==0) return arr[0];
-        if(idx<0) return 0;
-        if(dp[idx]!=null) return dp[idx];
-        int pick = arr[idx]+house(idx-2,arr,dp);
-        int not = 0+house(idx-1,arr,dp);
-        return dp[idx]= Math.max(not,pick);
+   private int dp(int nums[]){
+    int prev2=0;
+    int prev=nums[0];
+    int curri =0;
+    for(int i =1;i<nums.length;i++){
+        int take = nums[i];
+        if(i>1) take+=prev2;
+        int nottake = 0+ prev;
+        curri=Math.max(take,nottake);
+        prev2=prev;
+        prev=curri;
     }
-     private int houseT(int idx , int [] arr,Integer [] dp){
-        if(idx==1) return arr[1];
-        if(idx<=0) return 0;
-        if(dp[idx]!=null) return dp[idx];
-        int pick = arr[idx]+houseT(idx-2,arr,dp);
-        int not = 0+houseT(idx-1,arr,dp);
-        return  dp[idx] = Math.max(not,pick);
-    }
+    return prev;
+   }
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
-        Integer dp1 [] = new Integer[nums.length];
-        Integer dp2 [] = new Integer[nums.length];
-
-        int one = house(nums.length-2,nums,dp1); // leaving last element of array
-        int two = houseT(nums.length-1,nums,dp2); // leaving first element
+        int temp1 []= new int [nums.length-1];
+        int templ []= new int [nums.length-1];
+       for(int i =0;i<nums.length;i++){
+            if(i!=0) temp1[i-1] = nums[i];
+            if(i!=nums.length-1) templ[i]=nums[i];
+       }
+       int one = dp(temp1);
+       int two = dp(templ);
         return Math.max(one,two);
     }
 }
