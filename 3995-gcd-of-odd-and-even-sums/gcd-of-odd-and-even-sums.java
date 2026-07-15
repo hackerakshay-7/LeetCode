59186936558a57ce393dc47d7cd63b3f0f9e88a4
Akshat <akz.ggtv@gmail.com>
2026-07-15 10:19:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (20.94%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,12 @@
+class Solution {
+   // private int hcf(){}
+    public int gcdOfOddEvenSums(int n) {
+    //    int odd = (n/2)*(2+(n-1)*2);
+    //    int even =(n/2)*(4+(n-1)*2);
+    //    hcf(odd,even); 
+    // sum of odd = n/2(2 + 2n-2) -> n^2
+    // sum even = n/2(4+2n-2) -> (1+n)(n)
+    // both terms have n in common and 1+n cannot have any common factor as n how come?
+    return n;
+    }
+}
