6d86167d11d79c554fe91e87862c9f1d852affe3
Akshat <akz.ggtv@gmail.com>
2026-07-16 02:54:39 +0530

    Time: 5 ms (37.86%) | Memory: 47 MB (5.71%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -6,21 +6,9 @@ class Solution {
     // }
 
     public boolean canSortArray(int[] nums) {
-        // lets brute it cuz constraints are small
-        // 0(n) for bit count and for every n num -> complexity = bitlen*0(n)
-        // set meh store kiya jaaye cuz even if no repeats the set bit will be same
-        // let j>i -> if(arr[j]<arr[i] && set bit) swap if not break cuz adjacent
-        // at last use a for loop to check validity 
-        // and we are GTG
-        int bits[] = new int[nums.length];
-        for(int i =0;i<nums.length;i++){
-                int count =0,copy = nums[i];
-                while(copy>0){ if((copy & 1)==1) count++; copy = copy>>1;}
-                bits[i]=count;
-            }
             for(int i =0;i<nums.length;){
                 int j =i;
-                while(j<bits.length && bits[j]==bits[i]){j++;}
+                while(j<nums.length && Integer.bitCount(nums[i])==Integer.bitCount(nums[j])){j++;}
                 Arrays.sort(nums,i,j);
                 i=j;
             }
