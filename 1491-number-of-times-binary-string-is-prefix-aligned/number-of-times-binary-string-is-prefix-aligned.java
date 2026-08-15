c42d69109224dbda85ae00a8185e114b6ebcaf47
Akshat <akz.ggtv@gmail.com>
2026-08-15 12:07:20 +0530

    Time: 3 ms (45.95%) | Memory: 53 MB (31.08%) - LeetSync

--- 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.java
+++ 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.java
@@ -1,14 +1,11 @@
 class Solution {
     public int numTimesAllBlue(int[] flips) {
-        // just check if complete permutation
+        // i+1 ==changes
+        int maxvis = 0;
         int count =0;
-        int sum =0;
-        int numcount =0;
         for(int i =0;i<flips.length;i++){
-            numcount++;
-            sum+=flips[i];
-            if(sum==((float)(i+1)*(float)(i+2)/2) && numcount == i+1) count++;
+            maxvis = Math.max(maxvis,flips[i]);
+            if(i+1==maxvis) count++;
         }
-    return count;
-       }
+    return count;}
 }
